e0923a0c42abc9bad90775b4f7f33183a1f0bfa8
Mente <>
2024-04-25 00:53:54 -0400

    chap1

--- main.go
+++ main.go
@@ -2,23 +2,25 @@ package main
 
 import (
 	"net/http"
-	"os"
 )
 
 func main() {
     mux := http.NewServeMux()
     corsMux := middlewareCors(mux)
 
+    mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(200)
+        w.Write([]byte("OK"))
+    }) 
+
     server := http.Server{
         Addr: "localhost:3005",
         Handler: corsMux,
     }
 
-    err := server.ListenAndServe()
-
-    if err != nil {
-        os.Exit(0)
-    }
+    server.ListenAndServe()
 }
 
 func middlewareCors(next http.Handler) http.Handler {
